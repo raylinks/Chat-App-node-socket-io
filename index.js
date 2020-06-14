ad38b0541303d8e316d4dc8ff46f357f0efc48b6
rilwan <olawuyirilwan98@gmail.com>
2020-06-14 15:47:54 +0100

    helper functions user .js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const socketio = require('socket.io')
 const http = require('http');
 
+const { addUser , removeUser , getUser , getUsersInRoom}  = require('./src/users.js');
 
 const PORT = process.env.port || 5000
 
@@ -10,6 +11,26 @@ const router = require('./src/router');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log('we have a new collection');
+
+    socket.on('join', ({name, room }, callback) => {
+        console.log(name , room);
+
+        // const error = true;
+        // if(error){
+        //     callback({error: 'error'})
+        // }
+
+        callback();
+    })
+
+    socket.on('disconnect', () => {
+        console.log('User had left');
+    })
+
+})
 
 app.use(router);
 
