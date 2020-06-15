7fa71eea1d8aa4eb77f5a967b7951c7b846c7bed
rilwan <olawuyirilwan98@gmail.com>
2020-06-15 23:11:00 +0100

    socket io

--- index.js
+++ index.js
@@ -13,16 +13,23 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-    console.log('we have a new collection');
 
     socket.on('join', ({name, room }, callback) => {
-        console.log(name , room);
+    const {error , user } = addUser({ id : socket.id , name , room });
 
-        // const error = true;
-        // if(error){
-        //     callback({error: 'error'})
-        // }
+    if(error) return  callback(error);
 
+    socket.emit('message', { user: 'admin', text: `${user.name}, Welcome to the room  ${user.room}`})
+    socket.broadcast.to(user.room).emit('message', {user :'Admin', text : `${user.name}, has joined!`})
+    socket.join(user.room);
+    callback();
+    });
+
+
+    socket.on('sendMessage',(message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', {user:user.name, text:message});
         callback();
     })
 
